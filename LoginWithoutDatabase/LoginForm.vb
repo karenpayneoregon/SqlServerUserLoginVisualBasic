2854f9142eb40f8430e4a765a43e1ab6db15011f
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-15 08:56:41 -0800

    added code sample for forum question

---
+++ LoginWithoutDatabase/LoginForm.vb
@@ -0,0 +1,27 @@
+﻿Public Class LoginForm
+    Private userID As String = "spicyramen06"
+    Private password As String = "96619"
+    Public Shared attempts As Integer = 0
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+
+        attempts += 1
+
+        If attempts >= 4 Then
+            LoginButton.Enabled = False
+            MessageBox.Show("Attempts exceeded")
+            Exit Sub
+        End If
+        If Not String.IsNullOrWhiteSpace(UserNameTextBox.Text) AndAlso Not String.IsNullOrWhiteSpace(PasswordTextBox.Text) Then
+            If UserNameTextBox.Text = userID AndAlso PasswordTextBox.Text = password Then
+                MessageBox.Show("Welcome")
+            Else
+                Controls.OfType(Of TextBox).ToList().ForEach(Sub(tb) tb.Text = "")
+                MessageBox.Show("Invalid logn")
+            End If
+        End If
+    End Sub
+
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancelButton.Click
+        Close()
+    End Sub
+End Class
---
+++ LoginWithoutDatabase/readme.md
@@ -0,0 +1,5 @@
+﻿# About
+
+Raw pattern without a database or encryption along with user details in private variables within a form so this is not something one would use for production.
+
+![screen](login.png)
